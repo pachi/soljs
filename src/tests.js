ced937830bbd8709076083e511fc9fc9eb4bc776
Rafael Villar Burke <pachi@rvburke.com>
2016-11-01 23:06:32 +0100

    soljs: actualiza valores CTE

--- src/tests.js
+++ src/tests.js
@@ -323,7 +323,7 @@ console.log(check('Daily diffuse to total radiation ratio H_d_mean / H_mean (ZC1
                   sol.monthly_Id_to_I(sol.CTE_KTmean('1', false), sol.CTE_WSPENINSULA), 0.38, 2));
 console.log(check('Sunset hour angle for CTE_LATCANARIAS and July 17 (ws) [deg]',
                   sol.CTE_WSCANARIAS, 102.0, 1));
-console.log(check('Daily diffuse to total radiation ratio H_d_mean/H_mean (ZC1, Canarias)',
+console.log(check('Daily diffuse to total radiation ratio H_d_mean / H_mean (ZC1, Canarias)',
                   sol.monthly_Id_to_I(sol.CTE_KTmean('1', true), sol.CTE_WSCANARIAS), 0.38, 2));
 
 // Orientaciones
